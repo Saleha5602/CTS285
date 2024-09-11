15232dab59e04a4dbe32abac0559d6956e218462
Alya Saleh <aasaleh3@ncsu.edu>
2024-09-11 12:55:59 -0400

    added the template folder

--- m2t1/hello.py
+++ m2t1/hello.py
@@ -1,17 +1,13 @@
 # minimal Flask app
-from flask import Flask # type: ignore
+from flask import Flask, render_template # type: ignore
 #print("starting flask program", __name__)
 app = Flask(__name__)
 # do any app specific setup here
 #for instance, loding a database
 @app.route("/")
 def index():
-    return """<h1>Hello World</h1>
-    <h3>--------------------------</h3> 
-    <p>Name: Alya Saleh</p> 
-    <p>Class: CTS285</p>
-    <a href="action">Click here</a>
-    """
+    name= "Alya"
+    return render_template("main_page.html", name=name)
 @app.route("/action")
 def action():
     return "Hello from the action route!"
