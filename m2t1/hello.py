aafa259c71c665378fb8c6e643a90b26099371aa
Alya Saleh <aasaleh3@ncsu.edu>
2024-09-11 12:31:20 -0400

    added 2 route

--- m2t1/hello.py
+++ m2t1/hello.py
@@ -4,8 +4,13 @@ from flask import Flask # type: ignore
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
     return """<h1>Hello World</h1>
     <h3>--------------------------</h3> 
     <p>Name: Alya Saleh</p> 
-    <p>Class: CTS285</p>"""
+    <p>Class: CTS285</p>
+    <a href="action">Click here</a>
+    """
+@app.route("/action")
+def action():
+    return "Hello from the action route!"
